2c03ef1c09d1607cfacf50ed13ae4dbed5bc6069
RALD <yr.alditya@gmail.com>
2024-02-22 04:16:49 +0700

    Hook useEffect missing dependency solved by declare the function inside the Hook

--- src/App.js
+++ src/App.js
@@ -7,38 +7,28 @@ function App() {
   const [datas, setDatas] = useState([])
   const [selected, setSelected] = useState("")
   const [labelSelected, setLabelSelected] = useState("");
-  const [isShow, setIsShow] = useState(false)
   const [baseExp, setBaseExp] = useState(0)
   
-  const getPokemons = async () => {
-    const pokemons = await fetch("https://pokeapi.co/api/v2/pokemon/")
-    const value = await pokemons.json()
-    const results = value.results.map(data => {
-      return {
-        label: data.name
-          .toLowerCase()
-          .split(" ")
-          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-          .join(" "),
-        value: data.url,
-      };
-    })
-    setDatas(results.sort((a, b) => a.label > b.label ? 1 : -1))
-    // setDatas(results.sort((a, b) => a.label.localeCompare(b.localeCompare)))
-    // console.log(results)
-    console.log(datas)
-  }
+  
   useEffect(() => {
+    const getPokemons = async () => {
+      const pokemons = await fetch("https://pokeapi.co/api/v2/pokemon/")
+      const value = await pokemons.json()
+      const results = value.results.map(data => {
+        return {
+          label: data.name
+            .toLowerCase()
+            .split(" ")
+            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(" "),
+          value: data.url,
+        };
+      })
+      setDatas(results.sort((a, b) => a.label > b.label ? 1 : -1))
+      // setDatas(results.sort((a, b) => a.label.localeCompare(b.localeCompare)))
+    }
     getPokemons()
   }, [])
-
-  // const handleSubmit = async () => {
-  //   console.log(selected)
-  //   setIsShow(state => !state)
-  //   const detail = await fetch(selected)
-  //   const value = await detail.json()
-  //   setBaseExp(value.base_experience)
-  // }
 
   const handleChange = async (event) => {
     if (event) {
@@ -69,22 +59,11 @@ function App() {
           isClearable={true}
           onChange={(e) => handleChange(e)}
         />
-        {/* <button className="button" onClick={() => handleSubmit()} >
-          <code>{isShow ? "Hide Value" : "Show Value"}</code>
-        </button> */}
         <h1>
           <code>
             {selected ? `You choose ${labelSelected} (${baseExp} exp)` : ""}
           </code>
         </h1>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
     </div>
   );
